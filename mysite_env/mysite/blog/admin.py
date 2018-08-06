d0187549c82d045b07b5b1eda0cf90758e16f6f4
test <test@example.com>
2018-08-07 03:09:58 +0800

    update template nesting & add CSS effect for Mysite

--- mysite_env/mysite/blog/admin.py
+++ mysite_env/mysite/blog/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import BlogType, Blog
 
-# Register your models here.
-
 @admin.register(BlogType)
 class BlogTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'type_name')
 
 @admin.register(Blog)
-class Blog(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'create_time', 'update_last_time')
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog_type', 'author', 'create_time', 'update_last_time')
--- mysite_env/mysite/blog/urls.py
+++ mysite_env/mysite/blog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
+from .views import blog_list, blog_detail, blog_with_type
 
-# start with blog
 urlpatterns = [
-    # http://localhost:8000/blog/1
-    path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
-    path('type/<int:blog_type_pk>', views.blogs_with_type, name="blogs_with_type"),
+    path('', blog_list, name='blog_list'),
+    path('<int:blog_pk>', blog_detail, name='blog_detail'),
+    path('type/<int:blog_type_pk>', blog_with_type, name='blog_with_type'),
 ]

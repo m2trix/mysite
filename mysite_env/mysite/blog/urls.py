603cade343556d92c82bce093d0f59d298448810
test <test@example.com>
2018-08-12 02:31:46 +0800

    update bootstrap effect for Mysite UI

--- mysite_env/mysite/blog/urls.py
+++ mysite_env/mysite/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import blog_list, blog_detail, blog_with_type
+from . import views
 
 urlpatterns = [
-    path('', blog_list, name='blog_list'),
-    path('<int:blog_pk>', blog_detail, name='blog_detail'),
-    path('type/<int:blog_type_pk>', blog_with_type, name='blog_with_type'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
+    path('type/<int:blog_type_pk>', views.blog_with_type, name='blog_with_type'),
 ]
--- mysite_env/mysite/blog/views.py
+++ mysite_env/mysite/blog/views.py
@@ -4,6 +4,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 def blog_detail(request, blog_pk):
@@ -15,5 +16,6 @@ def blog_with_type(request, blog_type_pk):
     context = {}
     type_name = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=type_name)
-    context['blog_type'] = type_name
+    context['type_name'] = type_name
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_with_type.html', context)

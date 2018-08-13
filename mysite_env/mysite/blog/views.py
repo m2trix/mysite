591b7831600283097804fb1245d3996ef2f60f23
m2trix <m2trix@qq.com>
2018-08-14 02:56:58 +0800

    1. add ording for blog list 2. add Pagintor

--- mysite_env/mysite/blog/views.py
+++ mysite_env/mysite/blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 def blog_list(request):
+    # localhost:8000/blog?page=[page_num]
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10) # 每 10 个元素进行分页
+    page_num = request.GET.get('page', 1) # 获取 URL 的页面参数（GET请求）
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 

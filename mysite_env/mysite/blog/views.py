7696a6d1fec2e41bae51e5d2187da2569c6a7fc9
m2trix <m2trix@qq.com>
2018-08-15 02:45:26 +0800

    optimize paginator for mysite

--- mysite_env/mysite/blog/views.py
+++ mysite_env/mysite/blog/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.paginator import Paginator
+from django.conf import settings
 from .models import Blog, BlogType
+
+# each_page_blogs_number = 2
 
 def blog_list(request):
     # localhost:8000/blog?page=[page_num]
     blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list, 10) # 每 10 个元素进行分页
+    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER) # 每 X 个元素进行分页
     page_num = request.GET.get('page', 1) # 获取 URL 的页面参数（GET请求）
     page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number # 获取当前页码
+    # 获取当前页码前后各2页的页码范围
+    page_range = list(range( max(current_page_num-2, 1), current_page_num )) + \
+                 list(range( current_page_num, min(current_page_num+2, paginator.num_pages)+1 ))
+    # 加省略页码标记
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
 
     context = {}
+    context['blogs'] = page_of_blogs.object_list
+    context['page_range'] = page_range
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
@@ -22,7 +41,30 @@ def blog_detail(request, blog_pk):
 def blog_with_type(request, blog_type_pk):
     context = {}
     type_name = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=type_name)
+    blogs_all_list = Blog.objects.filter(blog_type=type_name)
+    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER) # 每 X 个元素进行分页
+    page_num = request.GET.get('page', 1) # 获取 URL 的页面参数（GET请求）
+    page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number # 获取当前页码
+    # 获取当前页码前后各2页的页码范围
+    page_range = list(range( max(current_page_num-2, 1), current_page_num )) + \
+                 list(range( current_page_num, min(current_page_num+2, paginator.num_pages)+1 ))
+    # 加省略页码标记
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context = {}
     context['type_name'] = type_name
+    context['blogs'] = page_of_blogs.object_list
+    context['page_range'] = page_range
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
+
     return render_to_response('blog/blog_with_type.html', context)

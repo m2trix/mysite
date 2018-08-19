1585d80e7219263b413d95bbee32c72dca79c49e
m2trix <m2trix@qq.com>
2018-08-19 23:58:56 +0800

    add RichTextEditor for mysite

--- mysite_env/mysite/blog/models.py
+++ mysite_env/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15)
@@ -10,7 +11,7 @@ class BlogType(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
-    content = models.TextField()
+    content = RichTextField()
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     create_time = models.DateTimeField(auto_now_add=True)
     update_last_time = models.DateTimeField(auto_now=True)
